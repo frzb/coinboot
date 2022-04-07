d9a7aed31caaa01ef2a3310b310f3babf0b6dd37
gm <gunter.miegel@rgsqd.de>
2022-04-07 09:27:21 +0200

    Restructure tests, cookstlye linter suggestions

--- server/test/coinboot_node_spec.rb
+++ server/test/coinboot_node_spec.rb
@@ -1,7 +1,8 @@
-control "coinboot-node-1" do
+# Please always use the cookstyle linter
+control 'coinboot-plugin' do
   impact 1.0
-  title "Coinboot node tests"
-  desc "Baseline testing for Coinbot node functionality"
+  title 'Coinboot node tests'
+  desc 'Baseline testing for Coinbot node functionality'
 
   describe directory('/home/ubuntu/test_dir') do
     its('owner') { should eq 'ubuntu' }
@@ -13,20 +14,37 @@ control "coinboot-node-1" do
     its('group') { should eq 'ubuntu' }
     its('content') { should match 'This is a test' }
   end
+end
 
-  describe command('lsb_release -d') do
-    its('exit_status') { should eq 0 }
-    its('stdout') { should include 'Description:	Ubuntu 20.04.4 LTS' }
-  end
+control 'coinboot-kernel' do
+  impact 1.0
+  title 'Coinboot node Kernel version'
+  desc 'Verify the Kernel version running on the Coinboot worker node'
 
   describe command('uname -r') do
     its('exit_status') { should eq 0 }
     its('stdout') { should include '5.11.0-46-generic' }
   end
+end
+
+control 'coinboot-distribution' do
+  impact 1.0
+  title 'Coinboot node Distribution release'
+  desc 'Verify the distribution release running on the Coinboot worker node'
+
+  describe command('lsb_release -d') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should include 'Description:	Ubuntu 20.04.4 LTS' }
+  end
+end
+
+control 'coinboot-zram' do
+  impact 1.0
+  title 'Coinboot node ZRAM RAM Compresssion'
+  desc 'Verify the ZSTD compressed ramdrive used for the RootFS'
 
   describe command('zramctl') do
     its('exit_status') { should eq 0 }
     its('stdout') { should include '/dev/zram0' }
   end
-
 end
